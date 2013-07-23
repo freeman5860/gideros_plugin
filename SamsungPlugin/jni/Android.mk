3c0799add0817cb523178bcd5067355089125925
freeman5860 <freeman5860@gmail.com>
2013-07-23 18:02:07 +0800

    change 0723

--- SamsungPlugin/jni/Android.mk
+++ SamsungPlugin/jni/Android.mk
@@ -11,26 +11,6 @@ LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libgideros.so
 include $(PREBUILT_SHARED_LIBRARY)
  
 #
-# Lua Socket Library
-#
-include $(CLEAR_VARS)
- 
-LOCAL_MODULE            := luasocket
-LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/libluasocket.so
- 
-include $(PREBUILT_SHARED_LIBRARY)
- 
-#
-# Lua File System Library
-#
-include $(CLEAR_VARS)
- 
-LOCAL_MODULE            := lfs
-LOCAL_SRC_FILES         := ../../libs/$(TARGET_ARCH_ABI)/liblfs.so
- 
-include $(PREBUILT_SHARED_LIBRARY)
- 
-#
 # Plugin
 #
 include $(CLEAR_VARS)
@@ -38,6 +18,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE           := samsungiap
 LOCAL_ARM_MODE         := arm
 LOCAL_CFLAGS           := -O2
+LOCAL_C_INCLUDES       += $(LOCAL_PATH)/../gideros/ $(LOCAL_PATH)/
 LOCAL_SRC_FILES        := samsungiap.cpp
 LOCAL_LDLIBS           := -ldl -llog
 LOCAL_SHARED_LIBRARIES := gideros
